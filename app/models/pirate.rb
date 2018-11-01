c077f640307fa600888e22aad20da93c818ea5fc
github username <github email address>
2018-11-01 10:53:30 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@all = []
+
+  def initialize(hashy)
+    @name = hashy[:name]
+    @weight = hashy[:weight]
+    @height = hashy[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+  @@all = []
+
+  attr_accessor :name, :type, :booty
+
+  def initialize(hashy)
+    @name = hashy[:name]
+    @type = hashy[:type]
+    @booty = hashy[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
 end
